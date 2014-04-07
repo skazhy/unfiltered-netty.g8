bf53b4758b5e4da5a26ff0e4608b92899cc30cbc
Karlis Lauva <skazhy@gmail.com>
2014-04-07 13:45:42 -0600

    Add minimal rx example

--- src/main/g8/src/main/scala/$package$/Server.scala
+++ src/main/g8/src/main/scala/$package$/Server.scala
@@ -6,7 +6,7 @@ object Server {
   def main(args: Array[String]) {
     unfiltered.netty.Http(8080)
       .handler(IndexPlan)
-      .run(_ => unfiltered.util.Browser.open(s"http://127.0.0.1:8080"))
+      .run()
   }
 
 }
